5a4052ef9b0efe01be87ea7163320c9359c012bb
mohyeonMan <112916714+mohyeonMan@users.noreply.github.com>
2022-11-20 16:50:08 +0900

    221120

---
+++ mini_project/src/main/java/product/bean/ProductDTO.java
@@ -0,0 +1,23 @@
+package product.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter@Getter
+public class ProductDTO {
+	private String subject;
+	private String category;
+	private String content;
+	private int hopeprice;
+	private int startprice;
+	private int unitprice;
+	private int trade_way;
+	private int enddays_year;
+	private int enddays_month;
+	private int enddays_day;
+	private int enddays_hour;
+	private int enddays_min;
+	private String id;
+	private int product_seq;
+	
+}
