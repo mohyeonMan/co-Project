d17bf123d286d57fa728087834fd0b2f96a30793
yongshik <yongshik@naver.com>
2022-11-21 14:21:04 +0900

    11/21

--- team_project/src/main/java/com/bit/team_project/controller/MainController.java
+++ team_project/src/main/java/com/bit/team_project/controller/MainController.java
@@ -1,10 +1,9 @@
 package com.bit.team_project.controller;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Component
+
 @Controller
 public class MainController {
 	@GetMapping(value = "/")
